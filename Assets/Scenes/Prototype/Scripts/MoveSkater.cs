c25af6b632b651a53fd37503527b1dfc3e59dcc0
Shunn Shine <jack.shunn@gmail.com>
2022-04-23 11:35:58 -0600

    Added a checkbox for movement with mouse

--- Assets/Scenes/Prototype/Scripts/MoveSkater.cs
+++ Assets/Scenes/Prototype/Scripts/MoveSkater.cs
@@ -6,13 +6,33 @@ public class MoveSkater : MonoBehaviour
 {
     public float speed;
     public float turnSpeed;
+    public bool mouseControl;
 
     // Update is called once per frame
     void Update()
     {
-        float turn = -Input.GetAxis("Horizontal");
-        transform.eulerAngles += new Vector3(0,0,turn*turnSpeed);
-        float rotation = Mathf.Deg2Rad*transform.eulerAngles[2];
-        transform.position += new Vector3(Mathf.Cos(rotation),Mathf.Sin(rotation), 0) * speed;
+        if (mouseControl)
+        {
+            //Debug.Log("MouseLocation" + Camera.main.ScreenToWorldPoint(Input.mousePosition) + "Diamond"+ transform.position);
+            Vector3 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition)+ new Vector3(0,0,10) - transform.position;
+            float rotation = Mathf.Deg2Rad * transform.eulerAngles[2];
+            Vector3 directionVector = new Vector3(Mathf.Sin(rotation), -Mathf.Cos(rotation), 0);
+            float angleToMouse = Vector3.Dot(directionVector, mouseVector.normalized);
+
+            int turn = angleToMouse > 0 ? -1 : 1;
+
+            transform.eulerAngles += new Vector3(0, 0, turn * turnSpeed);
+            rotation = Mathf.Deg2Rad * transform.eulerAngles[2];
+            transform.position += new Vector3(Mathf.Cos(rotation), Mathf.Sin(rotation), 0) * speed;
+
+            //Debug.Log("Direction:"+directionVector + "MouseVector:"+ mouseVector + "Angle to Mouse" + angleToMouse);
+        }
+        else
+        {
+            float turn = -Input.GetAxis("Horizontal");
+            transform.eulerAngles += new Vector3(0, 0, turn * turnSpeed);
+            float rotation = Mathf.Deg2Rad * transform.eulerAngles[2];
+            transform.position += new Vector3(Mathf.Cos(rotation), Mathf.Sin(rotation), 0) * speed;
+        }
     }
 }
