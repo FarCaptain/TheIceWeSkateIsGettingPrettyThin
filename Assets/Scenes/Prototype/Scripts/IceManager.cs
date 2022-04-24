81cc91c74fc1b0c323a655ecad576c066b5e493b
Sphinx <quyuan2008@163.com>
2022-04-23 21:25:56 -0600

    encircle mechanics

--- Assets/Scenes/Prototype/Scripts/IceManager.cs
+++ Assets/Scenes/Prototype/Scripts/IceManager.cs
@@ -44,14 +44,20 @@ public class IceManager : MonoBehaviour
     private Dictionary<string, int> nextTile;
     private TileBase[] allTiles;
 
+    private Dictionary<Vector3Int, bool> visitTable;
+
 
     private Vector3Int currentTile;
     private Grid grid;
+
+    private int[] dx = { 1, -1, 0, 0, -1, 1, -1, 1};
+    private int[] dy = { 0, 0, -1, 1, -1, -1, 1, 1};
 
     // Start is called before the first frame update
     void Start()
     {
         nextTile = new Dictionary<string, int>();
+        visitTable = new Dictionary<Vector3Int, bool>();
 
         allTiles = new TileBase[] { tile0, tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9, tile10, tile11, tile12 };
 
@@ -68,12 +74,16 @@ public class IceManager : MonoBehaviour
 
         grid = iceMap.layoutGrid;
         currentTile = grid.WorldToCell(Skater.transform.position);
+
+        //test
+        print("size:" + iceMap.size.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3Int cellLocation = grid.WorldToCell(Skater.transform.position);
+
         if (iceMap.HasTile(cellLocation) && currentTile != cellLocation)
         {
             int indexOfNextTile = nextTile[iceMap.GetTile(currentTile).name];
@@ -85,8 +95,70 @@ public class IceManager : MonoBehaviour
             else
             {
                 iceMap.SetTile(currentTile, allTiles[indexOfNextTile]);
+                for (int i = 0; i < 4; i++)
+                {
+                    Vector3Int adjacentTile = new Vector3Int(currentTile.x + dx[i], currentTile.y + dy[i], currentTile.z);
+
+                    //if(iceMap.HasTile(adjacentTile))
+                    //iceMap.SetTile(adjacentTile, tile6);
+
+                    if (iceMap.HasTile(adjacentTile) && !IsSteped(adjacentTile))
+                    {
+                        visitTable.Clear();
+                        if (findEncirledGrids(adjacentTile))
+                        {
+                            foreach (var pair in visitTable)
+                            {
+                                //set to water tiles
+                                Vector3Int tile = pair.Key;
+                                if (iceMap.HasTile(tile))
+                                    iceMap.SetTile(tile, allTiles[allTiles.Length - 1]);
+                            }
+                        }
+                    }
+                }
             }
             currentTile = cellLocation;
         }
+    }
+
+    private bool IsWater(Vector3Int tile)
+    {
+        int indexOfNextTile = nextTile[iceMap.GetTile(tile).name];
+        return indexOfNextTile == -1;
+    }
+
+    private bool IsSteped(Vector3Int tile)
+    {
+        return iceMap.GetTile(tile).name != allTiles[0].name;
+    }
+
+    //dfs
+    private bool findEncirledGrids(Vector3Int tile)
+    {
+
+        if (!iceMap.HasTile(tile))
+            return false;
+
+        if (IsSteped(tile))
+        {
+            if (visitTable.ContainsKey(tile))
+                visitTable.Remove(tile);
+            return true;
+        }
+
+        visitTable[tile] = true;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3Int adjacentTile = new Vector3Int(tile.x + dx[i], tile.y + dy[i], tile.z);
+
+            if (visitTable.ContainsKey(adjacentTile))
+                continue;
+
+            if (findEncirledGrids(adjacentTile) == false)
+                return false;
+        }
+        return true;
     }
 }
