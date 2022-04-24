fbd4ffd596704208d66360980a0564eb53b68813
Sphinx <quyuan2008@163.com>
2022-04-24 12:56:32 -0600

    following camera

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public bool followRotation = false;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+        if (followRotation)
+            transform.rotation = target.transform.rotation;
+    }
+}
