7a84af3e72a31339851e010e6c022b42bac61f4b
Sphinx <quyuan2008@163.com>
2022-10-21 03:25:43 -0600

    Add Audio Manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clips[UnityEngine.Random.Range(0, s.clips.Count)];
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+
+    //this addition to the code was made by me, the rest was from Brackeys tutorial
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Stop();
+    }
+}
---
+++ Assets/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    //public AudioClip clip;
+    public List<AudioClip> clips;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
